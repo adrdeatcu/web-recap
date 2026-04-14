df0d73764ff2428eca8d57358c8907783905f2f8
adrdeatcu <150624496+adrdeatcu@users.noreply.github.com>
2026-04-14 20:16:33 +0300

    reset button - reseting div's background

--- Etch-a-Sketch/myscript.js
+++ Etch-a-Sketch/myscript.js
@@ -1,5 +1,7 @@
 let container = document.querySelector("#container");
 const gridSize = document.querySelector("#gridSize");
+const colorMode = document.querySelector("#colorMode");
+const resetBtn = document.querySelector("#resetBtn");
 
 function createGrid(size = 16) {
     for(let i=1; i<=size * size; i++) {
@@ -18,15 +20,31 @@ function colorOnEnter(myDiv) {
     myDiv.style.backgroundColor = "black";
 }
 
+function randomColor(myDiv) {
+    
+}
+
 gridSize.addEventListener("click" , () => {
     let gridSizeInput = Number(prompt("Enter a grid number: maximum 64(64x64)"));
     container.textContent = '';
     createGrid(gridSizeInput);
 });
 
-function randomColor(myDiv) {
+colorMode.addEventListener("click", () =>{
     
-}
+});
+
+resetBtn.addEventListener("click", () =>{
+    let allGridBoxes = document.querySelectorAll(".gridBox");
+    console.log(allGridBoxes);
+    for (let i = 0; i < allGridBoxes.length; i++) {
+        let box = allGridBoxes[i];
+        box.style.backgroundColor = "white";
+    }
+        
+});
+
+
 
 
 createGrid();
