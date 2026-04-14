131287b0b87e8304851ce15212842f135a311e12
adrdeatcu <150624496+adrdeatcu@users.noreply.github.com>
2026-04-14 20:50:01 +0300

    random color function + defining a currentState in the project so we can switch black-color mode more easier on the same eventListener

--- Etch-a-Sketch/myscript.js
+++ Etch-a-Sketch/myscript.js
@@ -1,7 +1,10 @@
 let container = document.querySelector("#container");
 const gridSize = document.querySelector("#gridSize");
 const colorMode = document.querySelector("#colorMode");
+const blackBtn = document.querySelector("#blackBtn");
 const resetBtn = document.querySelector("#resetBtn");
+
+let currentMode = "black"; //choosed as default option
 
 function createGrid(size = 16) {
     for(let i=1; i<=size * size; i++) {
@@ -10,7 +13,12 @@ function createGrid(size = 16) {
         newDiv.style.width = `${700 / size}px`;
         newDiv.style.height = `${700 /size}px`;
         newDiv.addEventListener("mouseenter", (e) =>{
-            e.target.style.backgroundColor = "black";
+            if(currentMode === "black") {
+                e.target.style.backgroundColor = "black";
+            }else {
+                e.target.style.backgroundColor = randomColor();
+            }
+           
         });
         container.appendChild(newDiv);
     }
@@ -20,8 +28,15 @@ function colorOnEnter(myDiv) {
     myDiv.style.backgroundColor = "black";
 }
 
-function randomColor(myDiv) {
-    
+function randomColor() {
+    let colorArray = [];
+    for(let i=0; i<3; i++) {
+        colorArray[i] = Math.floor(Math.random() * 256);
+    }
+    let red = colorArray[0];
+    let green = colorArray[1];
+    let blue = colorArray [2];
+    return `rgb(${red} , ${green}, ${blue})`;
 }
 
 gridSize.addEventListener("click" , () => {
@@ -31,12 +46,16 @@ gridSize.addEventListener("click" , () => {
 });
 
 colorMode.addEventListener("click", () =>{
-    
+    currentMode = "color";
 });
+
+blackBtn.addEventListener("click", () =>{
+    currentMode = "black";
+});
+
 
 resetBtn.addEventListener("click", () =>{
     let allGridBoxes = document.querySelectorAll(".gridBox");
-    console.log(allGridBoxes);
     for (let i = 0; i < allGridBoxes.length; i++) {
         let box = allGridBoxes[i];
         box.style.backgroundColor = "white";
