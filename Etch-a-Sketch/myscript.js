56dd838170af607e0299228d5a8750fc6755619e
adrdeatcu <150624496+adrdeatcu@users.noreply.github.com>
2026-04-14 18:19:49 +0300

    generating the 16x16 grid using only js + mouseenter function to color the boxes

--- Etch-a-Sketch/myscript.js
+++ Etch-a-Sketch/myscript.js
@@ -1,1 +1,20 @@
-console.log(1)
+let container = document.querySelector("#container");
+
+function createGrid() {
+    for(let i=1; i<=256; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("gridBox");
+        newDiv.addEventListener("mouseenter", () =>{
+            colorOnEnter(newDiv);
+        });
+        container.appendChild(newDiv);
+    }
+}
+
+function colorOnEnter(myDiv) {
+    myDiv.style.backgroundColor = "black";
+}
+
+
+
+createGrid();
